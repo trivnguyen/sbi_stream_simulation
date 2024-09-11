6cc1d7b41dac2c8f483b85c3be08d14473bd42fc
Tri Nguyen <tnguyen@ccalin011.flatironinstitute.org>
2024-09-11 10:43:48 -0400

    update example config git status

--- configs/example_config.py
+++ configs/example_config.py
@@ -9,6 +9,7 @@ def get_config():
     config.binary_dir = './binaries'
     config.potential_dir = './pot'
     config.save_observables_only = False
+    config.overwrite = True
 
     # simulation configuration
     config.num_samples = 2  # set to a small number for testing
@@ -16,14 +17,14 @@ def get_config():
     # prior configuration
     config.priors = priors = config_dict.ConfigDict()
     priors.M_sat = dict(dist='log_uniform', a=1e-5, b=1e-1) # 1e10 Msun
-    priors.r_sat = dict(dist='log_uniform', a=1e-2, b=3)  # kpc  # match NFW profile 
+    priors.rs_sat = dict(dist='log_uniform', a=1e-2, b=3)  # kpc  # match NFW profile
     priors.vz = dict(dist='normal', loc=0, scale=100)  # km/s
     priors.vphi = dict(dist='normal', loc=0, scale=100)  # km/s
-    priors.r = dict(dist='uniform', loc=0, scale=3)  # 0 - few kpc  # 3 = high 
+    priors.r = dict(dist='uniform', loc=0, scale=3)  # 0 - few kpc  # 3 = high
     priors.phi = dict(dist='uniform', loc=0, scale=360.)  # TODO: periodic parameters
     # TODO: change phi_a to use 90th percentile stream length
     priors.phi_a = dict(dist='uniform', loc=-40, scale=80)  # impact point w.r.t prog
     priors.t_a = dict(dist='uniform', loc=0, scale=5)  # kpc/km/s  # difficult one # 5 = integration time
     priors.tmax = dict(dist='delta', value=4)  # integration time, leave at 4
-    
+
     return config
